9bbdde9f262a3d28d748035c4435cd0e84ba39d0
asudoh <asudoh@gmail.com>
2014-02-19 14:36:18 +0900

    More Travis CI integration.

--- tests/intern.js
+++ tests/intern.js
@@ -1,16 +1,18 @@
 // To run the test cases:
 //     With node.js:
-//         > cd /path/to/liaison
-//         > node node_modules/intern/bin/intern-client.js config=tests/intern
+//         > cd /path/to/liaison/
+//         > cd ..
+//         > node liaison/node_modules/intern/bin/intern-client.js config=liaison/tests/intern
 //     With browser: http://yourserver/path/to/liaison/node_modules/intern/client.html?config=tests/intern
 define({
 	loader: {
-		baseUrl: typeof window !== "undefined" ? "../../.." : "..",
+		baseUrl: typeof window !== "undefined" ? "../../.." : undefined,
 		packages: ["liaison"]
 	},
 	useLoader: {
 		"host-node": "dojo/dojo",
 		"host-browser": "../../../requirejs/require.js"
 	},
-	suites: ["liaison/tests/all"]
+	suites: ["liaison/tests/all"],
+	excludeInstrumentation: /^((liaison(\/|\\)(node_modules|tests))|requirejs|dojo|dcl)/
 });
