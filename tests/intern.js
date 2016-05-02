c501771b7b04dd2aed005519628602240cab91e8
Akira Sudoh <asudoh@gmail.com>
2016-05-02 17:49:16 +0900

    Update tested browser list.

--- tests/intern.js
+++ tests/intern.js
@@ -29,20 +29,20 @@ define({
 		{browserName: "internet explorer", version: "11", platform: "Windows 8.1", name: "liaison"},
 		{browserName: "firefox", version: "25", platform: [/*"OS X 10.6",*/ "Linux", "Windows 7"], name: "liaison"},
 		{browserName: "chrome", version: "", platform: [/*"OS X 10.6", */ "Linux", "Windows 7"], name: "liaison"},
-		{browserName: "safari", version: "6", platform: "OS X 10.8", name: "liaison"},
-		{browserName: "safari", version: "7", platform: "OS X 10.9", name: "liaison"},
+		{browserName: "safari", version: "8", platform: "OS X 10.10", name: "liaison"},
+		{browserName: "safari", version: "9.1", platform: "OS X 10.11", name: "liaison"},
 		// {browserName: "android", platform: "Linux", version: "4.1", name: "liaison"},
 		// {browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.1", name: "liaison"},
 		// {browserName: "android", platform: "Linux", version: "4.1", name: "liaison"},
 		// {browserName: "android", platform: "Linux", "device-type": "tablet", version: "4.0", name: "liaison"},
 		// {browserName: "android", platform: "Linux", version: "4.0", name: "liaison"},
 		// Non-empty selenium-version causes "browser failed to start" error for unknown reason
-		{browserName: "iphone", platform: "OS X 10.9", version: "7", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
-		{browserName: "ipad", platform: "OS X 10.9", version: "7", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
+		{browserName: "iphone", platform: "OS X 10.11", version: "9.1", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
+		{browserName: "ipad", platform: "OS X 10.11", version: "9.1", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
 		// {browserName: "iphone", platform: "OS X 10.8", version: "6.0", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
 		// {browserName: "ipad", platform: "OS X 10.8", version: "6.0", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
-		{browserName: "iphone", platform: "OS X 10.8", version: "6.1", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
-		{browserName: "ipad", platform: "OS X 10.8", version: "6.1", "device-orientation": "portrait", "selenium-version": "", name: "liaison"}
+		{browserName: "iphone", platform: "OS X 10.10", version: "8", "device-orientation": "portrait", "selenium-version": "", name: "liaison"},
+		{browserName: "ipad", platform: "OS X 10.10", version: "8", "device-orientation": "portrait", "selenium-version": "", name: "liaison"}
 	],
 
 	maxConcurrency: 3,
